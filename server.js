30309d4dba3d6205c470d2e8daa0d641ba98d2cf
brianleroux <brian@westcoastlogic.com>
2011-01-07 16:17:47 -0800

    more fucking around

--- server.js
+++ server.js
@@ -2,8 +2,33 @@ require.paths.unshift('./node_modules')
 var express = require('express')
   , app = express.createServer()
 
+
+app.configure(function(){
+    app.use(express.methodOverride());
+    app.use(express.bodyDecoder());
+    app.use(app.router);
+    app.use(express.staticProvider(__dirname + '/public'));
+})
+
+app.configure('development', function(){
+    app.use(express.errorHandler({ dumpExceptions: true, showStack: true }))
+    app.use(express.logger())
+})
+
+app.configure('production', function(){
+    app.use(express.errorHandler());
+})
+
 app.get('/', function (req, res) {
-    res.send('Hello World')
+    res.render('index.html.ejs')
+})
+
+app.get('/downloads', function (req, res) {
+    res.render('downloads.html.ejs')
+})
+
+app.get('/license', function (req, res) {
+    res.render('license.html.ejs')
 })
 
 app.listen(80)
