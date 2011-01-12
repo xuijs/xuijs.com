4ffafb2bd4f4d1b864cc5b5cbfdcc6a6659871a0
Brock Whitten <brock@sintaxi.com>
2011-01-11 21:33:30 -0800

    added current status to links. added .gitignore file

--- server.js
+++ server.js
@@ -2,21 +2,30 @@ var app = require('./config').app
 var port = process.env.PORT || 4000
 
 app.get('/', function (req, res) {
-    res.render('index.html.ejs')
+    res.render('index.html.ejs',{
+      locals: { current: "index" }
+    })
 })
 
 app.get('/downloads', function (req, res) {
-    res.render('downloads.html.ejs')
+    res.render('downloads.html.ejs',{
+      locals: { current: "downloads" }
+    })
 })
 
 app.get('/license', function (req, res) {
-    res.render('license.html.ejs')
+    res.render('license.html.ejs', {
+      locals: { current: "license" }
+    })
 })
 
 app.get('/docs/:doc?', function (req, res) {
    var doc = req.params.doc || 'index'
    res.render('docs/' + doc + '.ejs', {
-      locals: { type: 'docs' }
+      locals: { 
+        type: 'docs',
+        current: doc
+      }
    })
 })
 
